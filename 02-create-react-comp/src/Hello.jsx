c3e76429366373255b1b95b660f3ca7b1d055f85
hafijur <hafijur0211@gmial.com>
2024-02-08 15:49:53 +0530

    number add

--- 02-create-react-comp/src/Hello.jsx
+++ 02-create-react-comp/src/Hello.jsx
@@ -1,11 +1,12 @@
 function Hello() {
 
     let myName = "Hafijur"
+    let number = 34
     let fullName = ()=>{
         return 'hafijur rahman'
     }
 
-    return <h3>Hello ji My name is {fullName}</h3>;
+    return <h3>Hello ji My name is {fullName} and number is {number}</h3>;
 }
 
 export default Hello;

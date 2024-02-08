162d3ce1da51a18f702b8a5c95afc03240f865e2
hafijur <hafijur0211@gmial.com>
2024-02-08 21:41:24 +0530

    grid

--- 04-todo-app-version-1/src/App.jsx
+++ 04-todo-app-version-1/src/App.jsx
@@ -2,6 +2,13 @@ function App() {
   return (
     <center className="todo-container">
       <h1>TODO App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">col-8</div>
+          <div className="col-4">col-4</div>
+          <div className="col-2">col-4</div>
+        </div>
+      </div>
     </center>
   );
 }

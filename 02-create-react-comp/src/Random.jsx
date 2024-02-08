6f9240238ca4866a1f77c7485f513f84752ba51d
hafijur <hafijur0211@gmial.com>
2024-02-08 16:09:34 +0530

    style

--- 02-create-react-comp/src/Random.jsx
+++ 02-create-react-comp/src/Random.jsx
@@ -1,6 +1,6 @@
 function Random() {
     let number = Math.random() * 100;
-    return <h1>Random number is : {Math.round(number)}</h1>;
+    return <h1 style={{'backgroundColor':'#776643'}} >Random number is : {Math.round(number)}</h1>;
 }
 
 export default Random;

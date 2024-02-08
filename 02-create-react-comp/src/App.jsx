3a4b0a6f4cab9a85389ec16af49af0c57633ff7e
hafijur <hafijur0211@gmial.com>
2024-02-08 16:04:53 +0530

    random number

--- 02-create-react-comp/src/App.jsx
+++ 02-create-react-comp/src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from "./Hello";
+import Random from "./Random";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
       <Hello />
+      <Random/>
     </div>
   );
 }

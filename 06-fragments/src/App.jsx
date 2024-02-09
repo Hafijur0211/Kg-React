67ea6f764da2384edf398ab6abe6047551a0829f
hafijur <hafijur0211@gmial.com>
2024-02-09 21:35:16 +0530

    fragments

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -2,16 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">Dal</li>
+        <li className="list-group-item">Green Vegetable</li>
+        <li className="list-group-item">Roti</li>
+        <li className="list-group-item">Salad</li>
+        <li className="list-group-item">Milk</li>
       </ul>
-    </div>
+    </>
   );
 }
 

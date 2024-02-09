b1091db46118bd2572021b157e81a1af8b8060a1
hafijur <hafijur0211@gmial.com>
2024-02-09 13:51:30 +0530

    appjsx

--- 04-todo-app-version-1/src/App.jsx
+++ 04-todo-app-version-1/src/App.jsx
@@ -5,11 +5,41 @@ function App() {
     <center className="todo-container">
       <h1>Todo App</h1>
       <div className="container text-center">
+        
         <div className="row">
-          <div className="col-6">col-8</div>
-          <div className="col-4">col-4</div>
-          <div className="col-2">col-4</div>
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-success">
+              Add
+            </button>
+          </div>
         </div>
+
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">09/02/2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">09/02/2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
       </div>
     </center>
   );

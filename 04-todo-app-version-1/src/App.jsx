8922cb7c994ad502cdfb97cf33d7c9ca51d99a1b
hafijur <hafijur0211@gmial.com>
2024-02-09 09:06:19 +0530

    grid

--- 04-todo-app-version-1/src/App.jsx
+++ 04-todo-app-version-1/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>placeholder</div>;
+  return (
+    <center className="todo-container">
+      <h1>Todo App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">col-8</div>
+          <div className="col-4">col-4</div>
+          <div className="col-2">col-4</div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;

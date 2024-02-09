49388db603601e96c1feae1a4feb41a813d2a765
hafijur <hafijur0211@gmial.com>
2024-02-09 22:01:59 +0530

    key

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 
 function App() {
-  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
+  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
         {foodItems.map((item) => (
-          <li className="list-group-item">{item}</li>
+          <li key={item} className="list-group-item">{item}</li>
         ))}
       </ul>
     </>

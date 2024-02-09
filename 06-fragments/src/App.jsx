c5957c1109348a31572200e4e477a1413400434e
hafijur <hafijur0211@gmial.com>
2024-02-09 21:51:10 +0530

    fooditems

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 
 function App() {
+  let foodItems = ['Dal', 'Green Vegetables', 'Roti', 'Salad', 'Milk'];
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
-        <li className="list-group-item">Dal</li>
-        <li className="list-group-item">Green Vegetable</li>
-        <li className="list-group-item">Roti</li>
-        <li className="list-group-item">Salad</li>
-        <li className="list-group-item">Milk</li>
+        <li className="list-group-item">Dal</li> 
       </ul>
     </>
   );

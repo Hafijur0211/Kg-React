d6581a75b894a88dacee8ecdfcee266b30c824f9
hafijur <hafijur0211@gmial.com>
2024-02-09 21:26:06 +0530

    healthy food

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -1,8 +1,18 @@
-import './App.css'
+import "./App.css";
 
-function App() { 
-
-  return <div>Fragments</div>
+function App() {
+  return (
+    <div>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;

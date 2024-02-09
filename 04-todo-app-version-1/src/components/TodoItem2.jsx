06a45313ff9eb1bde6ac90665f93851f57beb7fd
hafijur <hafijur0211@gmial.com>
2024-02-09 19:42:40 +0530

    todoName and toDate

--- 04-todo-app-version-1/src/components/TodoItem2.jsx
+++ 04-todo-app-version-1/src/components/TodoItem2.jsx
@@ -1,9 +1,11 @@
 function TodoItem2() {
+    let todoName = "Go to college";
+    let todoDate = "09/02/2024";
     return (
         <div className="container text-center">
             <div className="row">
-                <div className="col-6">Go to college</div>
-                <div className="col-4">09/02/2024</div>
+                <div className="col-6">{todoName}</div>
+                <div className="col-4">{todoDate}</div>
                 <div className="col-2">
                     <button type="button" className="btn btn-danger">
                         Delete

6d962f66b2cfb6f8b813e2bb38646071d135cbdf
hafijur <hafijur0211@gmial.com>
2024-02-11 15:03:55 +0530

    logical operator

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -3,13 +3,12 @@ import "./App.css";
 function App() {
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
   // let foodItems = [];
-  let emptyMessage = foodItems.length === 0 ? <h3>I am still hungry.</h3> : null
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {/* Ternary Operator */}
-      {emptyMessage}
+      {/* Logical operator */}
+      {foodItems.length === 0 && <h3>I am still hungry.</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">

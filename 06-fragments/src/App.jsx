75e4302394c06ba7fdf13e46121fc7db74d2a2b3
hafijur <hafijur0211@gmial.com>
2024-02-12 14:11:19 +0530

    conditional rendering

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  let foodItems = []
+
+  if(foodItems.length === 0 ){
+    return <h3>I am still hungry.</h3>
+  }
   return (
     <>
       <h1>Healthy Food</h1>

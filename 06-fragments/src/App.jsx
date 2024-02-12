76105ce5fbf8e8c5d32a4dff15c9ded3bf08fd63
hafijur <hafijur0211@gmial.com>
2024-02-12 14:15:08 +0530

    trtnary operator

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -3,13 +3,12 @@ import "./App.css";
 function App() {
   // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
   let foodItems = []
-
-  if(foodItems.length === 0 ){
-    return <h3>I am still hungry.</h3>
-  }
+ 
   return (
     <>
       <h1>Healthy Food</h1>
+      {/* Ternary Operator */}
+      {foodItems.length === 0 ? <h3>I am still hungry.</h3> : null}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">

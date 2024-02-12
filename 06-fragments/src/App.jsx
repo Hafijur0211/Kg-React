4c0d14f135f7c3c3445f38e57ca83ec76e9e4739
hafijur <hafijur0211@gmial.com>
2024-02-12 14:46:13 +0530

    fooditems

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -1,21 +1,16 @@
 import "./App.css";
+import FoodItems from "./components/FoodItems";
 
 function App() {
-  // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
-  let foodItems = [];
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  // let foodItems = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
       {/* Truthy Falsy Method */}
       {foodItems.length === 0 && <h3>I am still hungry.</h3>}
-      <ul className="list-group">
-        {foodItems.map((item) => (
-          <li key={item} className="list-group-item">
-            {item}
-          </li>
-        ))}
-      </ul>
+       <FoodItems/>
     </>
   );
 }

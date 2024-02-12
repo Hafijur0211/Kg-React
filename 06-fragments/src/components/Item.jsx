175bc0f0d20e24d666d94b312c5b322e8cdad74c
hafijur <hafijur0211@gmial.com>
2024-02-12 20:16:07 +0530

    css module

--- 06-fragments/src/components/Item.jsx
+++ 06-fragments/src/components/Item.jsx
@@ -1,11 +1,11 @@
+import styles from "./Item.module.css";
+
 const Item = ({ foodItem }) => {
-    return (
-    
-            <li className="list-group-item kg-item">
-                <span className="kg-span">{foodItem}</span>
-            </li>
-        
-    );
+  return (
+    <li className={`${styles["kg-item"]} list-group-item`}>
+      <span className={styles['kg-span']}>{foodItem}</span>
+    </li>
+  );
 };
 
 export default Item;

2b3651bda9c310004651b92acb0bd7760cdce5d2
hafijur <hafijur0211@gmial.com>
2024-02-14 21:42:51 +0530

    button styles

--- 08-calculator-version-1/src/App.jsx
+++ 08-calculator-version-1/src/App.jsx
@@ -1,16 +1,15 @@
 import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
-  return <div className={styles.calculator} >
-    <input type="text" className={styles.display} />
-    <div className={styles.buttonsContainer} >
-      <button className={styles.button} >C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <ButtonsContainer/>
     </div>
-  </div>
+  );
 }
 
 export default App;

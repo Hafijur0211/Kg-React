062830ce8a2974c4d1cc11d350efbd0e48c2f7de
hafijur <hafijur0211@gmial.com>
2024-02-14 21:49:24 +0530

    map method

--- 08-calculator-version-1/src/components/ButtonsContainer.jsx
+++ 08-calculator-version-1/src/components/ButtonsContainer.jsx
@@ -1,13 +1,36 @@
-import styles from './ButtonsContainer.module.css'
+import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = () =>{
-  const buttonNames = ['C', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '/', '=', '9', '0', '.' ]
-    return <div className={styles.buttonsContainer}>
-    <button className={styles.button}>C</button>
-    <button className={styles.button}>1</button>
-    <button className={styles.button}>2</button>
-    <button className={styles.button}>+</button>
-  </div>
-}
+const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map((buttonName) => 
+        <button className={styles.button}>{buttonName}</button>
+      )}
 
-export default ButtonsContainer
+      <button className={styles.button}>1</button>
+      <button className={styles.button}>2</button>
+      <button className={styles.button}>+</button>
+    </div>
+  );
+};
+
+export default ButtonsContainer;

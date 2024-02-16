19d42536720da614b2f47c398459f14d67001a2d
hafijur <hafijur0211@gmial.com>
2024-02-16 20:31:56 +0530

    handlebuybuttonclicked

--- 06-fragments/src/components/Item.jsx
+++ 06-fragments/src/components/Item.jsx
@@ -1,11 +1,16 @@
 import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
+
+  const handleBuyButtonClicked =(foodItem)=>{
+    console.log(`${foodItem} being bought`)
+  }
+
   return (
     <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={styles['kg-span']}>{foodItem}</span>
       <button className={`${styles.button} btn btn-info`}
-      onClick={()=> console.log(`${foodItem} being bought`)} >Buy</button>
+      onClick={()=> handleBuyButtonClicked(foodItem) } >Buy</button>
     </li>
     
   );

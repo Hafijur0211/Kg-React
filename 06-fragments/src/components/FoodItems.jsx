23242407d6790337d44193cffc1df7f745de0df0
hafijur <hafijur0211@gmial.com>
2024-02-16 21:22:47 +0530

    handlebuyclicked

--- 06-fragments/src/components/FoodItems.jsx
+++ 06-fragments/src/components/FoodItems.jsx
@@ -4,7 +4,11 @@ const FoodItems = ({ items }) => {
     return (
         <ul className="list-group">
             {items.map((item) => (
-                <Item key={item} foodItem={item} />
+                <Item
+                    key={item}
+                    foodItem={item}
+                    handleBuyButton={() => console.log("clicked")}
+                />
             ))}
         </ul>
     );

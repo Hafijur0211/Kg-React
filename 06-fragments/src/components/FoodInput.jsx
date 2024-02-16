6bb583f6f5af0421ba381b6e6ff60e3ccd007405
hafijur <hafijur0211@gmial.com>
2024-02-16 20:52:50 +0530

    styles module

--- 06-fragments/src/components/FoodInput.jsx
+++ 06-fragments/src/components/FoodInput.jsx
@@ -1,5 +1,13 @@
-const FoodInput = ()=>{
-    return <input type="text" />
-}
+import styles from "./FoodInput.module.css";
+
+const FoodInput = () => {
+    return (
+        <input
+            type="text"
+            placeholder="Enter Food Item Here"
+            className={styles.foodInput}
+        />
+    );
+};
 
 export default FoodInput;

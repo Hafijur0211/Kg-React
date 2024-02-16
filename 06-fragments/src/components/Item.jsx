b73b71f76b93982a6bd675a9d20be27ab71623c0
hafijur <hafijur0211@gmial.com>
2024-02-16 20:28:40 +0530

    fooditem

--- 06-fragments/src/components/Item.jsx
+++ 06-fragments/src/components/Item.jsx
@@ -4,7 +4,8 @@ const Item = ({ foodItem }) => {
   return (
     <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={styles['kg-span']}>{foodItem}</span>
-      <button className={`${styles.button} btn btn-info`} >Buy</button>
+      <button className={`${styles.button} btn btn-info`}
+      onClick={()=> console.log(`${foodItem} being bought`)} >Buy</button>
     </li>
     
   );

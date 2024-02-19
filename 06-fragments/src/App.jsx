dac8773041a5f0640f276d491706cc529ba64ee6
Hafijur Rahman <hafijur021998@gmail.com>
2024-02-18 20:49:06 -0800

    useState

--- 06-fragments/src/App.jsx
+++ 06-fragments/src/App.jsx
@@ -3,32 +3,34 @@ import ErrorMessage from "./components/ErrorMessage";
 import "./App.css";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
   let foodItems = ["Sabzi", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
   // let foodItems = [];
 
-  let textToShow = "Food Item Entered by User"
+  let textStateArr = useState("Food Item Entered by User");
+  let textToShow = textStateArr[0];
+  let setTextState = textStateArr[1];
 
   const handleOnChange = (event) => {
-    console.log(event.target.value)
-    textToShow = event.target.value
-}
+    console.log(event.target.value);
+    textToShow = event.target.value;
+  };
 
   return (
     <>
-    <Container>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items={foodItems} />
-      <FoodInput handleOnChange={handleOnChange} />
-      <p>{textToShow}</p>
-      <FoodItems items={foodItems} />
-    </Container>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodInput handleOnChange={handleOnChange} />
+        <p>{textToShow}</p>
+        <FoodItems items={foodItems} />
+      </Container>
 
-    {/* <Container>
+      {/* <Container>
       <p>Above is the list of healthy foods that are good for your health and well being. </p>
     </Container> */}
-    
     </>
   );
 }

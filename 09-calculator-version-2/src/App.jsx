de8a5ca38064cbb6f0686b2ae8cfa95fc7324307
Hafijur Rahman <hafijur021998@gmail.com>
2024-02-24 22:08:55 -0800

    readonly

--- 09-calculator-version-2/src/App.jsx
+++ 09-calculator-version-2/src/App.jsx
@@ -2,12 +2,15 @@ import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Display from "./components/Display";
 import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
 
 function App() {
+  let [calVal, setCalVal] = useState("");
+
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonsContainer/>
+      <Display displayValue={calVal} />
+      <ButtonsContainer />
     </div>
   );
 }
